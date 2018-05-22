a96a0be769182b80106378559075181f9c2afa98
Dhafa Hikmawan <32931992+nawamkihafahd@users.noreply.github.com>
2018-05-22 23:43:08 +0700

    db -> insert data (job, skill, course, equipment)

--- mbdfp.sql
+++ mbdfp.sql
@@ -19,7 +19,7 @@ DROP TABLE IF EXISTS `course`;
 CREATE TABLE `course` (
   `COURSE_ID` int(11) NOT NULL AUTO_INCREMENT,
   `SKILL_ID` int(11) DEFAULT NULL,
-  `COURSE_NAME` varchar(20) DEFAULT NULL,
+  `COURSE_NAME` varchar(30) DEFAULT NULL,
   `COURSE_DESCRIPTION` varchar(30) DEFAULT NULL,
   `COURSE_STAMINA` int(11) DEFAULT NULL,
   `COURSE_DURATION` int(11) DEFAULT NULL,
@@ -27,13 +27,21 @@ CREATE TABLE `course` (
   `COURSE_EXPERIENCE` int(11) DEFAULT NULL,
   PRIMARY KEY (`COURSE_ID`),
   KEY `FK_REFERENCE_20` (`SKILL_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 /*Data for the table `course` */
 
 insert  into `course`(`COURSE_ID`,`SKILL_ID`,`COURSE_NAME`,`COURSE_DESCRIPTION`,`COURSE_STAMINA`,`COURSE_DURATION`,`COURSE_COST`,`COURSE_EXPERIENCE`) values 
-(1,1,'C++ Learning',NULL,1,10,0.00,2),
-(2,5,'Database - mysql',NULL,3,60,300.00,20);
+(1,1,'C++ Learning','Acquire C++ Skill',1,10,0.00,5),
+(2,5,'Database - mysql Course','Acquire mysql Skill',3,60,300.00,15),
+(3,2,'Web Learning : html','Acquire html Skill',5,180,600.00,20),
+(4,3,'Web Learning : css','Acquire css Skill',6,300,900.00,25),
+(5,4,'Web Learning : php','Acquire php Skill',7,480,2400.00,30),
+(6,6,'Web Learning : bootstrap','Acquire bootstrap Skill',8,720,900.00,35),
+(7,8,'Graphic Design Course','Acquire Graphic Design Skill',6,1200,6000.00,20),
+(8,9,'Animation Course','Acquire Animation Skill',6,900,9000.00,20),
+(9,7,'Java Course','Acquire Java Skill',4,240,900.00,18),
+(10,10,'Game Engine Learning','Acquire Game Engine Skill',8,1080,10000.00,25);
 
 /*Table structure for table `equipment` */
 
@@ -41,7 +49,7 @@ DROP TABLE IF EXISTS `equipment`;
 
 CREATE TABLE `equipment` (
   `UPGRADE_ID` int(11) NOT NULL,
-  `UPGRADE_NAME` varchar(20) DEFAULT NULL,
+  `UPGRADE_NAME` varchar(40) DEFAULT NULL,
   `UPGRADE_COST` float(8,2) DEFAULT NULL,
   `UPGRADE_DESCRIPTION` varchar(25) DEFAULT NULL,
   `UPGRADE_TIME` int(11) DEFAULT NULL,
@@ -53,7 +61,12 @@ CREATE TABLE `equipment` (
 
 insert  into `equipment`(`UPGRADE_ID`,`UPGRADE_NAME`,`UPGRADE_COST`,`UPGRADE_DESCRIPTION`,`UPGRADE_TIME`,`UPGRADE_LEVELCOST`) values 
 (1,'Laptop',500.00,NULL,1,1000),
-(2,'External Storage',510.00,NULL,1,1500);
+(2,'External Storage',600.00,NULL,1,1200),
+(3,'RAM',1000.00,NULL,1,2000),
+(4,'VGA',2000.00,NULL,5,8000),
+(5,'Internet',1000.00,NULL,2,4000),
+(6,'Game Engine',4000.00,NULL,15,2000),
+(7,'Image Processing Software',800.00,NULL,10,3500);
 
 /*Table structure for table `job` */
 
@@ -61,19 +74,29 @@ DROP TABLE IF EXISTS `job`;
 
 CREATE TABLE `job` (
   `JOB_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `JOB_NAME` varchar(30) DEFAULT NULL,
-  `JOB_DESCRIPTION` varchar(50) DEFAULT NULL,
+  `JOB_NAME` varchar(40) DEFAULT NULL,
+  `JOB_DESCRIPTION` varchar(200) DEFAULT NULL,
   `JOB_EXPERIENCE` int(11) DEFAULT NULL,
   `JOB_PAYMENT` float(8,2) DEFAULT NULL,
   `JOB_STAMINA` int(11) DEFAULT NULL,
   `JOB_DURATION` int(11) DEFAULT NULL,
   PRIMARY KEY (`JOB_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 /*Data for the table `job` */
 
 insert  into `job`(`JOB_ID`,`JOB_NAME`,`JOB_DESCRIPTION`,`JOB_EXPERIENCE`,`JOB_PAYMENT`,`JOB_STAMINA`,`JOB_DURATION`) values 
-(1,'Hello World',NULL,10,30.00,1,10);
+(1,'Hello World',NULL,10,30.00,1,10),
+(2,'Simple Database',NULL,30,60.00,1,60),
+(3,'Web (html)',NULL,40,90.00,2,45),
+(4,'Web (css)',NULL,45,120.00,3,60),
+(5,'Web (bootstrap)',NULL,45,150.00,3,120),
+(6,'Web (database)',NULL,200,500.00,7,600),
+(7,'Game (Java)',NULL,100,250.00,7,1200),
+(8,'Game (web, database)',NULL,2000,2000.00,8,2400),
+(9,'Game (Engine)',NULL,4000,4000.00,10,6000),
+(10,'Logo Design',NULL,20,300.00,4,6000),
+(11,'Promotional Animation Design',NULL,20,500.00,6,6000);
 
 /*Table structure for table `job_equipment` */
 
@@ -92,7 +115,44 @@ CREATE TABLE `job_equipment` (
 /*Data for the table `job_equipment` */
 
 insert  into `job_equipment`(`JOB_ID`,`UPGRADE_ID`,`JOB_EQUIPMENT_LEVEL`) values 
-(1,1,1);
+(1,1,1),
+(2,1,2),
+(2,2,1),
+(3,1,3),
+(3,5,1),
+(4,1,3),
+(4,4,1),
+(4,5,1),
+(5,1,3),
+(5,4,1),
+(5,5,1),
+(6,1,5),
+(6,2,3),
+(6,3,2),
+(6,5,3),
+(7,1,5),
+(7,3,4),
+(7,4,3),
+(7,7,1),
+(8,1,6),
+(8,2,4),
+(8,3,4),
+(8,4,1),
+(8,5,3),
+(8,7,1),
+(9,1,9),
+(9,2,6),
+(9,3,6),
+(9,4,6),
+(9,6,1),
+(10,7,3),
+(10,1,7),
+(10,3,7),
+(10,4,6),
+(11,7,4),
+(11,1,8),
+(11,3,5),
+(11,4,5);
 
 /*Table structure for table `job_skill` */
 
@@ -110,7 +170,34 @@ CREATE TABLE `job_skill` (
 /*Data for the table `job_skill` */
 
 insert  into `job_skill`(`SKILL_ID`,`JOB_ID`) values 
-(1,1);
+(1,1),
+(5,2),
+(2,3),
+(2,4),
+(3,4),
+(2,5),
+(3,5),
+(6,5),
+(2,6),
+(3,6),
+(6,6),
+(5,6),
+(4,6),
+(7,7),
+(8,7),
+(9,7),
+(2,8),
+(3,8),
+(4,8),
+(5,8),
+(6,8),
+(8,8),
+(9,8),
+(8,9),
+(9,9),
+(10,9),
+(8,10),
+(9,11);
 
 /*Table structure for table `level_user` */
 
@@ -131,7 +218,12 @@ insert  into `level_user`(`LEVEL_ID`,`LEVEL_MAX_EXP`,`LEVEL_MAX_STAMINA`,`LEVEL_
 (2,200,10,1),
 (3,400,10,1),
 (4,800,10,1),
-(5,1600,10,1);
+(5,1600,11,1),
+(6,3200,12,1),
+(7,9600,16,1),
+(8,20000,18,2),
+(9,40000,20,2),
+(10,80000,22,2);
 
 /*Table structure for table `skill` */
 
@@ -141,7 +233,7 @@ CREATE TABLE `skill` (
   `SKILL_ID` int(11) NOT NULL AUTO_INCREMENT,
   `SKILL_NAME` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`SKILL_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 /*Data for the table `skill` */
 
@@ -151,7 +243,11 @@ insert  into `skill`(`SKILL_ID`,`SKILL_NAME`) values
 (3,'css'),
 (4,'php'),
 (5,'mysql'),
-(6,'bootstrap');
+(6,'bootstrap'),
+(7,'java'),
+(8,'Graphic Design'),
+(9,'Animation'),
+(10,'Game Engine');
 
 /*Table structure for table `user` */
 
@@ -170,7 +266,7 @@ CREATE TABLE `user` (
   PRIMARY KEY (`USER_ID`),
   KEY `LEVEL_ID` (`LEVEL_ID`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`LEVEL_ID`) REFERENCES `level_user` (`LEVEL_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
@@ -246,13 +342,15 @@ DELIMITER $$
 /*!50003 CREATE FUNCTION `fn_isjobavailable`(`f_usrid` INT, `f_jid` INT) RETURNS tinyint(1)
 BEGIN
 	call sp_updatestamina(f_usrid);
+	if(exists(select * from `user_job` where `JOB_ID` = f_jid and `USER_ID` = f_usrid)) then
+	return 0;
 	select `USER_STAMINA` from `user` where `USER_ID` = f_usrid into @s0;
 	select `JOB_STAMINA` from `job` where `JOB_ID` = f_jid into @s1;
 	select count(*) from `user_skill` a, `job_skill` b where b.`JOB_ID` = f_jid and a.`USER_ID` = f_usrid and b.`SKILL_ID` = a.`SKILL_ID` into @n0;
 	select count(*) from `job_skill` where `JOB_ID` = f_jid into @n1;
 	select count(*) from `job_equipment` a, `user_equipment` b where a.`JOB_ID` = f_jid and b.`USER_ID` = f_usrid and a.`UPGRADE_ID` = b.`UPGRADE_ID` and (a.`JOB_EQUIPMENT_LEVEL` = b.`USER_EQUIPMENT_LEVEL` or a.`JOB_EQUIPMENT_LEVEL` < b.`USER_EQUIPMENT_LEVEL`) into @n2;
 	select count(*) from `job_equipment` where `JOB_ID` = f_jid into @n3;
-	if(@s0 < @s1) then
+	else if(@s0 < @s1) then
 		return 0;
 	else if(@n1 > @n0) then
 		return 0;
@@ -260,6 +358,7 @@ BEGIN
 		return 0;
 	else 
 		return 1;
+	end if;
 	end if;
 	end if;
 	end if;
@@ -337,6 +436,12 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_changepassword`(`p_uname` VARCHAR(50), `p_oldpass` VARCHAR(50), `p_newpass` VARCHAR(50))
 proc:BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	SELECT `USER_PASSWORD`, `USER_NAME` FROM `user` WHERE `USER_UNAME`=p_uname
 			INTO @password, @Name;
 					
@@ -351,6 +456,7 @@ proc:BEGIN
 		END IF;
 		UPDATE `user` SET `USER_PASSWORD`=MD5(p_newpass) WHERE `USER_UNAME`=p_uname;
 		SELECT 0,"ubah password SUKSES !";
+		commit;
 	END */$$
 DELIMITER ;
 
@@ -362,6 +468,12 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_levelupdate`(`p_usrid` INT, `p_dx` INT)
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	select `USER_EXP`, `LEVEL_ID` from `user` where `USER_ID` = p_usrid into @x0, @l0;
 	set @xt = @x0 + p_dx;
 	select `LEVEL_MAX_EXP` from `level_user` where `LEVEL_ID` = @l0 into @max;
@@ -370,6 +482,7 @@ BEGIN
 	else
 		UPDATE `user` SET `USER_EXP` = @xt - @max, `LEVEL_ID` = @l0 + 1 where `USER_ID` = p_usrid; 
 	end if;
+	commit;
 	END */$$
 DELIMITER ;
 
@@ -381,12 +494,19 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_login`(`p_uname` VARCHAR(50), `p_password` VARCHAR(50))
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	IF EXISTS(SELECT 1 FROM `user` 
 		WHERE `USER_UNAME`=p_uname AND `USER_PASSWORD`= MD5(p_password)) THEN
 		SELECT 0, "login sukses";
 	ELSE
 		SELECT -1, "maaf username / password tidak dikenal";	
 	END IF;
+	commit;
 	END */$$
 DELIMITER ;
 
@@ -398,6 +518,12 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_register`(`p_uname` VARCHAR(15), `p_password` VARCHAR(32), `p_name` VARCHAR(20))
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	IF NOT EXISTS (SELECT 1 FROM `user` WHERE `USER_UNAME`=p_uname) THEN
 		INSERT INTO `user` (`LEVEL_ID`, `USER_NAME`, `USER_UNAME`, `USER_PASSWORD`, `USER_EXP`, `USER_MONEY`, `USER_STAMINA`, `USER_LASTACTION`)  
 		VALUES (1, p_name, p_uname, MD5(p_password), 0, 100, 10, NOW());
@@ -405,6 +531,7 @@ BEGIN
 	ELSE 
 		SELECT -1, 'Gagal, username sudah terdaftar!';
 	END IF;
+	commit;
 	END */$$
 DELIMITER ;
 
@@ -416,6 +543,12 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_takecourse`(`p_usrid` INT, `p_courseid` INT)
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	CALL sp_updatestamina(p_usrid);
 	select `USER_MONEY`, `USER_STAMINA` from `user` where `USER_ID`=p_usrid
 	into @usrbal, @st;
@@ -437,6 +570,7 @@ BEGIN
 	end if;
 	end if;
 	end if;
+	commit;
 	END */$$
 DELIMITER ;
 
@@ -448,6 +582,12 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_takecoursereward`(`p_usrid` INT, `p_cid` INT)
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	if not exists(select `USER_COURSE_START` from `user_course` where `USER_ID` = p_usrid and `COURSE_ID` = p_cid) then
 		select -1, "Course not found";
 	else
@@ -466,6 +606,7 @@ BEGIN
 			select 0, "Course's reward successfully taken";
 		end if;
 	end if;
+	commit;
 	END */$$
 DELIMITER ;
 
@@ -477,6 +618,12 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_takejob`(`p_usrid` INT, `p_jid` INT)
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	call sp_updatestamina(p_usrid);
 	if(fn_isjobavailable(p_usrid, p_jid)) then
 		select `JOB_PAYMENT`, `JOB_STAMINA` from `job` into @dm, @ds;
@@ -487,6 +634,7 @@ BEGIN
 	else 
 		select -1, "Job unavailable";
 	end if;
+	commit;
 	END */$$
 DELIMITER ;
 
@@ -498,6 +646,12 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_takejobreward`(`p_usrid` INT, `p_jid` INT)
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	if not exists(select `USER_JOB_START` from `user_job` where `USER_ID` = p_usrid and `JOB_ID` = p_jid) then 
 		select -1, "Job Not Found";
 	else
@@ -517,7 +671,7 @@ BEGIN
 			select 0, "Job XP taken";
 		end if;
 	end if;
-	
+	commit;
 	END */$$
 DELIMITER ;
 
@@ -529,8 +683,15 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_updatestamina`(`p_usrid` INT)
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	set @st = fn_stamina(p_usrid);
 	update `user` set `USER_STAMINA` = @st, `USER_LASTACTION`=now() where `USER_ID` = p_usrid;
+	commit;
 	END */$$
 DELIMITER ;
 
@@ -542,6 +703,12 @@ DELIMITER $$
 
 /*!50003 CREATE PROCEDURE `sp_upgrade`(`p_usrid` INT, `p_upgrid` INT)
 BEGIN
+DECLARE EXIT HANDLER FOR SQLEXCEPTION, NOT FOUND
+BEGIN
+	ROLLBACK;
+	-- LEAVE proc;
+END;
+	start transaction;
 	select `USER_MONEY` from `user` where `USER_ID` = p_usrid into @usrbal;
 	set @cost = fn_upgradecost(p_upgrid, p_usrid);
 	if(@cost > @usrbal) then 
@@ -561,6 +728,7 @@ BEGIN
 		end if;
 		select 0, "upgrade success";
 	end if;
+	commit;
 	END */$$
 DELIMITER ;
 
